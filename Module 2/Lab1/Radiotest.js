08bee5712fe3284df4bd6694b0df9a3d44611bd3
LollipopYo <melodyting6688@msn.com>
2024-08-22 18:14:56 +1200

    upload

---
+++ Module 2/Lab1/Radiotest.js
@@ -0,0 +1,11 @@
+function submitform(e) {
+    e.preventDefault();
+
+    console.log(document.getElementsByName("JavaScript"));
+
+  }
+  
+  // function radioclick(radio) {
+  //   console.log(`Selected value: ${radio.value}`);
+  // }
+  
--- Module 2/Lab1/submission.js
+++ Module 2/Lab1/submission.js
@@ -3,11 +3,11 @@ function submitform(e) {
 
   console.log(document.getElementById("fname").value);
   console.log(document.getElementById("sname").value);
-  // console.log(document.getElementByid("radioclick").value);
+  console.log(document.getElementsByName("JavaScript"));
   // console.log(document.getElementById("checkbox").value);
   // console.log(document.getElementById("cars").value);
 }
 
-function radioclick(radio) {
-  console.log(`Selected value: ${radio.value}`);
-}
+// function radioclick(radio) {
+//   console.log(`Selected value: ${radio.value}`);
+// }
