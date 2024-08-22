e772aa1b9ce505523c50a8592fc661318c2a55e1
LollipopYo <melodyting6688@msn.com>
2024-08-22 17:51:06 +1200

    upload

--- Module 2/Lab1/submission.js
+++ Module 2/Lab1/submission.js
@@ -1,7 +1,13 @@
+function submitform(e) {
+  e.preventDefault();
 
-function submitform(e){
-e.preventDefault();
+  console.log(document.getElementById("fname").value);
+  console.log(document.getElementById("sname").value);
+  // console.log(document.getElementByid("radioclick").value);
+  // console.log(document.getElementById("checkbox").value);
+  // console.log(document.getElementById("cars").value);
+}
 
-
-console.log(document.getElementById("fname"));
+function radioclick(radio) {
+  console.log(`Selected value: ${radio.value}`);
 }
