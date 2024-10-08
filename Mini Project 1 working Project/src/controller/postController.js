13fd92ac6ca0300a736b8f7fbb088073e732187b
LollipopYo <melodyting6688@msn.com>
2024-10-08 22:04:07 +1300

    upload

---
+++ Mini Project 1 working Project/src/controller/postController.js
@@ -0,0 +1,25 @@
+const posts = [{title: "Test", body: "testing", id:1}];
+
+const getPosts = () => {
+    return posts;
+};
+
+const addPost = (post) => {
+  //find out max id in current posts
+  const nextId =
+    posts.length === 0 ? 1 : Math.max(...posts.map((post) => post.id)) + 1;
+  const resultPost = { ...post, id: nextId };
+  posts.push(resultPost);
+  return resultPost;
+};
+const deletePost= (id) => {
+    const idIndex = posts.map((post) => post.id).indexOf(id);
+    if(idIndex<0) throw new Error();
+    posts.splice(idIndex, 1);
+};
+
+module.exports = {
+  getPosts,
+  addPost,
+  deletePost,
+};
